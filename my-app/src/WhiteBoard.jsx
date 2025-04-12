5eaa3c0929f9db1becc7d2524fe2b15fe648ab92
unknown <manavijhalani@gmail.com>
2025-04-12 17:30:37 +0530

    multi page functionality

--- my-app/src/WhiteBoard.jsx
+++ my-app/src/WhiteBoard.jsx
@@ -14,11 +14,13 @@ import { Stack, TextField } from "@mui/material";
 import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
+import { useEffect } from "react";
+
 
 
 export default function CanvasPage() {
 
-const Canvasref=useRef(null);
+const Canvasref=useRef([React.createRef()]);//array of refs for each page to call related functions like save,undo etc
 const [boardcolor,setboardcolor]=useState("pink");
 const [brushColor,setbrushColor]=useState("black")
 const [colorpicker,showColorpicker]=useState(false);
@@ -26,6 +28,33 @@ const [brushRadius,setbrushRadius]=useState(10);
 const [radiusslider,setshowradiusslider]=useState(false);
 const [messages,setMessage]=useState([]);
 const [newInputMessage,setnewInputMessage]=useState("")
+const [pageindex,setpageindex]=useState(0);
+const [pagedrawing,savepagedrawing]=useState([])
+const [pagewiseboardcolor,setpagewiseboardcolor]=useState([])
+
+
+useEffect(() => {
+  if (Canvasref.current.length === 0) {
+    Canvasref.current.push(React.createRef());
+  }
+}, []);
+
+useEffect(()=>{
+  const canvas=Canvasref.current[pageindex]?.current;
+  if(canvas)
+  {
+       canvas.clear();
+       if(pagedrawing[pageindex])
+        {
+          setTimeout(() => {
+            canvas.loadSaveData(pagedrawing[pageindex], true);
+          }, 0);
+          console.log(pagedrawing[pageindex])
+        }
+  }
+  
+
+},[pageindex])
 
 const handleSendMessage=(e)=>{
   setMessage([
@@ -60,18 +89,31 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 const handleClear=()=>{
-    Canvasref.current.clear();
+    Canvasref.current[pageindex]?.current.clear();
 }
 
+const handleaddpage=()=>{
+  const newpage=React.createRef();
+  Canvasref.current.push(
+     newpage
+  )
+  setpageindex(pageindex+1)
 
-
-const handleSave=()=>{
-    const data=Canvasref.current.getSaveData();
-    console.log(data)
 }
+
+const handleSave=(pageindex)=>{
+  if (Canvasref.current[pageindex] && Canvasref.current[pageindex].current) {
+     const data=Canvasref.current[pageindex].current?.getSaveData();
+     const drawings=[...pagedrawing]
+     drawings[pageindex]=data
+    savepagedrawing(drawings)
+    const pageboardcolors=[...pagewiseboardcolor]
+    pageboardcolors[pageindex]=boardcolor
+    setpagewiseboardcolor(pageboardcolors)
+}}
 
 const handleUndo=()=>{
-  Canvasref.current.undo();
+  Canvasref.current[pageindex]?.current.undo();
 }
 
 const handleColorpicker=()=>{
@@ -81,13 +123,39 @@ const handleRadius=()=>{
     setshowradiusslider(!radiusslider)
 }
 
+const handlePrevious=()=>{
+  if(pageindex>0){
+  setpageindex(pageindex-1)
+    if(pagewiseboardcolor[pageindex])
+    {
+      setboardcolor(pagewiseboardcolor[pageindex])
+    }
+}
+else{
+  alert('you are on first page')
+}
+}
+
+const handleNext=()=>{
+  if(pageindex<Canvasref.current.length-1){
+  setpageindex(pageindex+1)
+  if(pagewiseboardcolor[pageindex])
+    {
+      setboardcolor(pagewiseboardcolor[pageindex])
+    }
+}
+else{
+  alert('you are on last page')
+}
+
+}
 
   return (
     <div>
     <div style={{ padding: "20px" }}>
       <h2>Draw Something!</h2>
       <Button variant="outlined" style={{margin:"20px"}} onClick={handleClear}>Clear</Button>
-      <Button variant="outlined" style={{margin:"20px"}} onClick={handleSave}>Save</Button>
+      <Button variant="outlined" style={{margin:"20px"}} onClick={()=>{handleSave(pageindex)}}>Save</Button>
       <Button variant="outlined" style={{margin:"20px"}} onClick={handleUndo}>Undo</Button>
       <Button variant="outlined" style={{margin:"20px"}} onClick={handleRadius}> Brush Radius</Button>
             {radiusslider && (
@@ -120,17 +188,22 @@ const handleRadius=()=>{
 
      <div style={{ display: 'flex', gap: '20px', marginTop: '30px', alignItems: 'flex-start' }}>
       <div style={{borderColor:'2px solid black', padding:'4px',display:'inline-block'}}>
-      <CanvasDraw ref={Canvasref}
+        <p>{pageindex+1}</p>
+       <CanvasDraw ref={Canvasref.current[pageindex]}
+        //key={pageindex}
         brushColor={brushColor}
         brushRadius={brushRadius}
         lazyRadius={2}
         canvasWidth={1000}
         canvasHeight={700}
         hideGrid={true}
-        backgroundColor={boardcolor}
+        backgroundColor={pagewiseboardcolor[pageindex]||boardcolor}
         enablePanAndZoom={false}
-        //disabled={true}
-      />
+        />
+      <Button sx={{marginTop:"10px"}} onClick={handlePrevious}>Previous Page</Button>
+      <Button sx={{marginTop:"10px"}} onClick={handleaddpage}>Add new page</Button>
+      <Button sx={{marginTop:"10px"}} onClick={handleNext}>Next Page</Button>
+
       </div>
       <div style={{display:'inline-block'}}>
         <Card>{card}</Card>
